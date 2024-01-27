9ed98e02b38c457558ab2b636c78e715626cab8b
Carl John Ignacio V. Nicor <cjinicor@gmail.com>
2024-01-27 13:45:41 +0800

    feat: added bullet prefab and shooting mechanic
    TODO:
    * Create "killzone" to kill bullet clones.
    * Create Arena

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,11 @@ public class PlayerController : MonoBehaviour
     private Vector2 _moveInput;
     private Vector2 _mousePos;
     [SerializeField] float playerSpeed;
+    [SerializeField] float bulletSpeed;
     private Rigidbody2D _rb2d;
     public Camera cam;
+    public GameObject bulletPrefab;
+    public Transform barrel;
 
     void Start()
     {
@@ -22,6 +25,11 @@ public class PlayerController : MonoBehaviour
         _moveInput.x = Input.GetAxisRaw("Horizontal");
         _moveInput.y = Input.GetAxisRaw("Vertical");
         _moveInput.Normalize();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 
     void FixedUpdate()
@@ -38,5 +46,12 @@ public class PlayerController : MonoBehaviour
         Vector2 lookDirection = _mousePos - _rb2d.position;
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
         _rb2d.rotation = angle;
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, barrel.position, barrel.rotation);
+        Rigidbody2D bullet_rB = bullet.GetComponent<Rigidbody2D>();
+        bullet_rB.AddForce(barrel.up * bulletSpeed, ForceMode2D.Impulse);
     }
 }
